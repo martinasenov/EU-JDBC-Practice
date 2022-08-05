4cd8ea26f4af75f3280265d999745afb8dcb4353
martin <martin.manuilov.asenov@gmail.com>
2022-08-05 14:37:26 +0200

    moving to next row with WHILE LOOP

--- src/test/java/jdbctests/TestConnection.java
+++ src/test/java/jdbctests/TestConnection.java
@@ -13,7 +13,7 @@ public class TestConnection {
         Connection connection= DriverManager.getConnection(dbUrl,dbUsername,dbPassword);
         Statement statement=connection.createStatement();
         ResultSet resultSet=statement.executeQuery("SELECT*FROM regions");
-
+/*
         //next is used to move the pointer to first row in the table
         resultSet.next();
 
@@ -23,8 +23,22 @@ public class TestConnection {
         //getting information with column index(starts 1)
         System.out.println(resultSet.getString(2));
 
+        //1-Europe
+        //2-Americas
 
+        System.out.println(resultSet.getInt(1) + " - " + resultSet.getString(2));
 
+        //move to second row
+        resultSet.next();
+        System.out.println(resultSet.getInt(1) + " - " + resultSet.getString(2));
+
+        //move to third row
+        resultSet.next();
+        System.out.println(resultSet.getInt(1) + " - " + resultSet.getString(2));
+*/
+        while (resultSet.next()){
+            System.out.println(resultSet.getInt(1) + " - " + resultSet.getString(2));
+        }
 
 
         //close connection
